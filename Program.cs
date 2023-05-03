84bff9ac3dbd68fa18778f549a5a598de50ec942
Sabrina Caldas Berno <sabrinaberno@hotmail.com>
2023-05-03 14:24:27 -0300

    Adicionando funcionalidades visuais no tabuleiro

--- Program.cs
+++ Program.cs
@@ -13,7 +13,9 @@ namespace jogo_de_xadrez{
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1,3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0,2));
 
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3,5));
                 Tela.imprimirTabuleiro(tab);
+
             } catch(TabuleiroException e){
                 Console.WriteLine(e.Message);
             }
