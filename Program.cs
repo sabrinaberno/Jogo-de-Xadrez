ca9adfc1987d7248aaa2e3c8a1c161ab1d40075e
Sabrina Caldas Berno <sabrinaberno@hotmail.com>
2023-05-04 09:59:48 -0300

    Criando movimento das peças

--- Program.cs
+++ Program.cs
@@ -8,10 +8,20 @@ namespace jogo_de_xadrez{
 
             try{
             PartidaDeXadrez partida = new PartidaDeXadrez();
-
                 
-            Tela.imprimirTabuleiro(partida.tab);
+                while(!partida.terminada){
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executarMovimento(origem, destino);
+                } 
+                       
             } catch(TabuleiroException e){
                 Console.WriteLine(e.Message);
             }
--- Tela.cs
+++ Tela.cs
@@ -1,5 +1,6 @@
 using System;
 using tabuleiro;
+using xadrez;
 
 namespace jogo_de_xadrez {
     class Tela {
@@ -19,6 +20,13 @@ namespace jogo_de_xadrez {
                 Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static PosicaoXadrez lerPosicaoXadrez () {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
         }
 
         public static void imprimirPeca(Peca peca) {
