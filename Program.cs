17de7127e7e2678418acf0b7e932a2068ad5e33d
Sabrina Caldas Berno <sabrinaberno@hotmail.com>
2023-05-04 09:45:54 -0300

    Instanciando as peças e começando as jogadas

--- Program.cs
+++ Program.cs
@@ -7,14 +7,10 @@ namespace jogo_de_xadrez{
         static void Main(string[] args) {
 
             try{
-                Tabuleiro tab = new Tabuleiro(8,8);
+            PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1,3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0,2));
-
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3,5));
-                Tela.imprimirTabuleiro(tab);
+                
+            Tela.imprimirTabuleiro(partida.tab);
 
             } catch(TabuleiroException e){
                 Console.WriteLine(e.Message);
