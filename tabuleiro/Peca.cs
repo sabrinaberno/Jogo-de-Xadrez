3a7160400407c1d656fb6c0bb5fbf83e962fb0fc
Sabrina Caldas Berno <sabrinaberno@hotmail.com>
2023-05-08 18:33:28 -0300

    Criando os movimentos

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -1,5 +1,5 @@
 namespace tabuleiro {
-    class Peca{
+    abstract class Peca {
         public Posicao posicao {get; set; }
         public Cor cor {get; protected set;}
         public int qtdMovimentos {get; protected set;}
@@ -12,8 +12,6 @@ namespace tabuleiro {
             this.qtdMovimentos = 0;
         }
 
-        public void incrementarQtdMovimentos () {
-            qtdMovimentos ++;
-        }
+        public abstract bool[,] movimentosPossiveis();
     }
 }
