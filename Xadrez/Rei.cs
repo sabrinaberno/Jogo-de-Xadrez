df28c242f7fe6578cbfa8bfd5dbdf9d392653cc7
Sabrina Caldas Berno <sabrinaberno@hotmail.com>
2023-05-09 09:38:33 -0300

    Corrigindo erros do movimento

--- Xadrez/Rei.cs
+++ Xadrez/Rei.cs
@@ -17,7 +17,7 @@ namespace xadrez {
             return p == null || p.cor != cor; 
         }
 
-        public  bool[,] movimentosPossiveis(){
+        public override bool[,] movimentosPossiveis(){
             bool[,] mat = new bool[tab.linhas, tab.colunas];
             Posicao pos = new Posicao(0,0);
             
