c1e5fbf2c450ae28543aadf59bf7e1e7a7d646af
Sabrina Caldas Berno <sabrinaberno@hotmail.com>
2023-05-09 10:42:03 -0300

    Corrigindo erros do movimento

--- Xadrez/Torre.cs
+++ Xadrez/Torre.cs
@@ -12,6 +12,11 @@ namespace xadrez {
             return "T";
         }
 
+        private bool podeMover(Posicao pos){
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor; 
+        }
+
         public override bool[,] movimentosPossiveis() {
             bool[,] mat = new bool[tab.linhas, tab.colunas];
             posicao pos = new Posicao(0,0);
@@ -43,17 +48,17 @@ namespace xadrez {
                 if(tab.peca(pos) != null && tab.peca(pos).cor != cor){
                     break;
                 }
-                pos.linha = pos.coluna + 1;
+                pos.coluna = pos.coluna + 1;
             }
 
-            //esuerda 
+            //esquerda 
             pos.definirValores(posicao.linha, posicao.coluna - 1);
             while (tab.posicaoValida(pos) && podeMover(pos)) {
                 mat[pos.linha, pos.coluna] = true;
                 if(tab.peca(pos) != null && tab.peca(pos).cor != cor){
                     break;
                 }
-                pos.linha = pos.coluna - 1;
+                pos.coluna = pos.coluna - 1;
             }
 
             return mat;
--- tabuleiro/Posicao.cs
+++ tabuleiro/Posicao.cs
@@ -8,6 +8,11 @@ namespace tabuleiro {
             this.coluna = coluna;
         }
 
+        public void definirValores(int linha, int coluna){
+            this.linha =  linha;
+            this.coluna = coluna;
+        }
+
         //rever o vídeo ToString
         public override string ToString() {
             return linha + ", " +  coluna;
