ccd9c89d0e7079e3978652bdacc40c7149f92d02
Sabrina Caldas Berno <sabrinaberno@hotmail.com>
2023-05-11 08:53:09 -0300

    Implementando as restrições de movimento

--- Program.cs
+++ Program.cs
@@ -11,12 +11,16 @@ namespace jogo_de_xadrez{
                 
                 while(!partida.terminada){
 
+                    try{
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine("Turno: " + partida.turno);
+                    Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
 
                     Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    partida.validarPosicaoDeOrigem(origem);
 
                     bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
                 
@@ -26,8 +30,14 @@ namespace jogo_de_xadrez{
                     Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                    partida.validarPosicaoDeDestino(origem, destino);
 
-                    partida.executarMovimento(origem, destino);
+                    partida.realizaJogada(origem, destino);
+
+                    } catch (TabuleiroException e) {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
                 } 
                        
             } catch(TabuleiroException e){
